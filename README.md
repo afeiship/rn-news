f070d6fac201844cb20ed055af65c79b317aa259
afeiship <1290657123@qq.com>
2017-07-26 18:22:24 +0800

    issue: update docs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rn-news
 > Rn news demo.
+
+
+## step by step:
+```bash
+react-native init --version="0.44.0" RnNews
+```
+
+## resources:
++ https://github.com/facebook/react-native/issues/14382
--- RnNews/index.ios.js
+++ RnNews/index.ios.js
@@ -9,24 +9,14 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  WebView,
   View
 } from 'react-native';
 
 export default class RnNews extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <WebView source={{uri: 'https://w.weipaitang.com/'}} />
     );
   }
 }
