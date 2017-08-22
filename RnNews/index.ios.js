0be052cac25c5674251393c57e3f98ac9deedd73
afeiship <1290657123@qq.com>
2017-08-23 00:39:00 +0800

    issue: share api

--- RnNews/index.ios.js
+++ RnNews/index.ios.js
@@ -13,6 +13,7 @@ import {
   WebView,
   StatusBar,
   Image,
+  Share,
   ActivityIndicator,
   Settings,
   View
@@ -37,12 +38,15 @@ import {
   Col,
   Row,
   CheckBox,
-  Toast,
   List,
   ListItem
 } from 'native-base';
 
-import {Row as row, Col as col} from 'react-native-styles-flexbox-grid';
+import {row,col} from 'react-native-styles-flexbox-grid';
+import SK from 'react-native-stylekit';
+import PTRView from 'react-native-pull-to-refresh';
+import Config from 'react-native-config'
+
 
 const styles = StyleSheet.create({
   container: {
@@ -81,9 +85,47 @@ const styles = StyleSheet.create({
 
 
 export default class RnNews extends Component {
+  _refresh() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 2000)
+    });
+  }
+
+  _onPress = e => {
+    Alert.alert(
+      'Alert Title',
+      'sdfsdf'+Config.API_URL,
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      { cancelable: false }
+    )
+  };
+
+  _onPress1 = e => {
+    Share.share({
+      message: 'A framework for building native apps using React',
+      url: 'http://facebook.github.io/react-native/',
+      title: 'React Native'
+    }, {
+      dialogTitle: 'Share React Native website',
+      excludedActivityTypes: [
+        'com.apple.UIKit.activity.PostToTwitter'
+      ],
+      tintColor: 'green'
+    })
+    .then(this._showResult)
+    .catch((error) => this.setState({result: 'error: ' + error.message}));
+  };
+
   render() {
     const items = ['Simon Mignolet', 'Nathaniel Clyne', 'Dejan Lovren', 'Mama Sakho', 'Emre Can'];
     return (
+
       <Container>
         <Header>
           <Body>
@@ -91,71 +133,71 @@ export default class RnNews extends Component {
           </Body>
         </Header>
 
-        <Content>
-          <Text style={[styles.welcome]}>Hello Native Base!</Text>
-          <Button full success onPress={()=> Toast.show({
-            text: 'Wrong password!',
-            position: 'bottom',
-            buttonText: 'Okay'
-          })}>
-            <Icon name='home'/>
-            <Text style={{color: '#fff'}}>Show Toast</Text>
-          </Button>
-          <Button full>
-            <Text style={{color: '#fff'}}>Native Base button</Text>
-          </Button>
-          <List dataArray={items}
-                renderRow={(item) =>
-                  <ListItem>
-                    <Text>{item}</Text>
-                  </ListItem>
-                }>
-          </List>
+        <PTRView onRefresh={this._refresh}>
 
-          <Card>
-            <CardItem header>
-              <Text>NativeBase</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-              <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-                     style={{width: 300, height: 300}} />
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>GeekyAnts</Text>
-            </CardItem>
-          </Card>
-
-          <ListItem>
-            <CheckBox checked={true} />
-            <Body>
-            <Text>Daily Stand Up</Text>
-            </Body>
-          </ListItem>
-          <ListItem>
-            <CheckBox checked={false} />
-            <Body>
-            <Text>Discussion with Client</Text>
-            </Body>
-          </ListItem>
-
-          <View style={[{backgroundColor: '#EEE', height: 100}, row.$, row.center]}>
-            <View style={[{backgroundColor: '#635DB7'}, col.$, col.span30]}>
+          <Content>
+            <Text style={[styles.welcome]}>Hello Native Base!</Text>
+            <Button full success onPress={this._onPress1}>
               <Icon name='home'/>
-            </View>
-            <View style={[{backgroundColor: '#00CE9F'}, col.$, col.span20]}>
-              <Icon name='ios-menu'/>
-            </View>
-          </View>
+              <Text style={{color: '#fff'}}>Show Toast</Text>
+            </Button>
+            <Button full>
+              <Text style={{color: '#fff'}}>Native Base button</Text>
+            </Button>
+            <List dataArray={items}
+                  renderRow={(item) =>
+                    <ListItem>
+                      <Text>{item}</Text>
+                    </ListItem>
+                  }>
+            </List>
 
-        </Content>
+            <Card>
+              <CardItem header>
+                <Text>NativeBase</Text>
+              </CardItem>
+              <CardItem>
+                <Body>
+                <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+                       style={{width: 300, height: 300}}/>
+                </Body>
+              </CardItem>
+              <CardItem footer>
+                <Text>GeekyAnts</Text>
+              </CardItem>
+            </Card>
+
+            <ListItem>
+              <CheckBox checked={true}/>
+              <Body>
+              <Text>Daily Stand Up</Text>
+              </Body>
+            </ListItem>
+            <ListItem>
+              <CheckBox checked={false}/>
+              <Body>
+              <Text>Discussion with Client</Text>
+              </Body>
+            </ListItem>
+
+            <View style={[{backgroundColor: '#EEE', height: 100}, row.$, row.center]}>
+              <Text style={[{backgroundColor: '#635DB7'}, col.$, SK.tc, SK.c_red]}>
+                <Icon name='home'/>
+              </Text>
+              <Text style={[{backgroundColor: '#00CE9F'}, col.span33, SK.tc]}  onPress={this._onPress} >
+                <Icon name='ios-menu' style={{ color:'#fff'}}/>
+              </Text>
+            </View>
+
+          </Content>
+        </PTRView>
         <Footer>
           <FooterTab>
             <Text>Footer Here!</Text>
           </FooterTab>
         </Footer>
       </Container>
+
     );
   }
 }
